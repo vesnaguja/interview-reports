2dcd98e1fdc9e09c14ab6ad6d5b94248c1747a79
ninamn87 <ivanamilenovic87@gmail.com>
2021-09-09 19:51:58 +0200

    create dummy main page - edit footer

--- src/app/shared/Footer/Footer.jsx
+++ src/app/shared/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="footer container-fluid bg-primary fixed-bottom">
-      <div className="container px-3 py-2 text-light">© 2021 Copyright BIT</div>
+      <div className="container px-3 py-2 text-light">© 2021 Copyright BIT - TEAM "All Pain, No Gain" </div>
     </div>
   );
 };
