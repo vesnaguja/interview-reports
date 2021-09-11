e6f549d2b93a4154c85a35ec25cec8bd23076720
ndjordjevic990 <n.djordjevic990@gmail.com>
2021-09-11 17:44:32 +0200

    initial branch commit

--- src/view/pages/SingleCandidate/CandidateReport/CandidateReport.jsx
+++ src/view/pages/SingleCandidate/CandidateReport/CandidateReport.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { Table, Container } from "react-bootstrap";
+import { getReports } from "../../../../services/service";
 
-const CandidateReport = () => {
+const CandidateReport = ({ token }) => {
+  let { id } = useParams("id");
+  const [reports, setReports] = useState([]);
+
+  useEffect(() => {
+    getReports(token, id).then((reportsData) => setReports(reportsData));
+  }, [token, id]);
+
   return (
     <Container>
       <h3 className="mt-4 mb-3">Reports:</h3>
@@ -15,9 +24,9 @@ const CandidateReport = () => {
         </thead>
         <tbody>
           <tr>
-            <td>Google</td>
-            <td>20.12.2017</td>
-            <td>Passed</td>
+            <td>{reports.companyName}</td>
+            <td>{reports.interviewDate}</td>
+            <td>{reports.status}</td>
             <td>Modul</td>
           </tr>
         </tbody>
