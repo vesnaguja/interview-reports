570bbdec39d83c32fd23c1e288116ba1f2e800aa
vesnaguja <80161546+vesnaguja@users.noreply.github.com>
2021-09-14 20:41:13 +0200

    make candidates button functional

--- src/view/components/Header/Header.jsx
+++ src/view/components/Header/Header.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Container, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <Container fluid className="bg-primary py-2">
-      <Container className="d-flex justify-content-between px-3">
+    <Container fluid className="bg-primary py-2 mb-5">
+      <Container className="d-flex justify-content-between">
         <h3 className="text-white">Interview Reports</h3>
-        <Button className="text-primary bg-light">Candidates</Button>
+        <Link to="/"> <Button className="text-primary bg-light">Candidates</Button> </Link>
       </Container>
     </Container>
   );
