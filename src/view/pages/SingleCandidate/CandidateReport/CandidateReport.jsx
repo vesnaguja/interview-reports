acbe95bda491538a2a5f72a744c9ffe5d3f9f4e9
ndjordjevic990 <n.djordjevic990@gmail.com>
2021-09-16 18:59:14 +0200

    add loader on all pages

--- src/view/pages/SingleCandidate/CandidateReport/CandidateReport.jsx
+++ src/view/pages/SingleCandidate/CandidateReport/CandidateReport.jsx
@@ -3,12 +3,16 @@ import { useParams } from "react-router-dom";
 import { Table, Container } from "react-bootstrap";
 import { getReports } from "../../../../services/service";
 import Report from "./Report";
+import Loader from "../../../components/Loader/Loader";
 
 const CandidateReport = ({ token }) => {
   let { id } = useParams("id");
   const [reports, setReports] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(false);
+
     getReports(token, id).then((reportsData) => {
       setReports(reportsData);
     });
@@ -17,20 +21,24 @@ const CandidateReport = ({ token }) => {
   return (
     <Container>
       <h3 className="mt-4 mb-3">Reports:</h3>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Company</th>
-            <th>Interview date</th>
-            <th colSpan="2">Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {reports.map((report) => {
-            return <Report report={report} key={report.id} />;
-          })}
-        </tbody>
-      </Table>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Company</th>
+              <th>Interview date</th>
+              <th colSpan="2">Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {reports.map((report) => {
+              return <Report report={report} key={report.id} />;
+            })}
+          </tbody>
+        </Table>
+      )}
     </Container>
   );
 };
