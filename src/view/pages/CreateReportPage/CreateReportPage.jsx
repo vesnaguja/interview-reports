ad2a3e1d390f9648d407f41a49f997989a704e64
ndjordjevic990 <n.djordjevic990@gmail.com>
2021-09-21 18:39:09 +0200

    create select candidate section

--- src/view/pages/CreateReportPage/CreateReportPage.jsx
+++ src/view/pages/CreateReportPage/CreateReportPage.jsx
@@ -4,37 +4,56 @@ import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import { Container, Col, Row } from "react-bootstrap";
 import SelectCandidate from "./SelectCandidate/SelectCandidate";
-import { Button } from "react-bootstrap";
-
+const newReportInitialState = {
+  id: null,
+  candidateId: null,
+  candidateName: null,
+  companyId: null,
+  companyName: null,
+  iterviewDate: null,
+  phase: null,
+  status: null,
+  note: null,
+};
 const CreateReportPage = () => {
   // const token = localStorage.getItem("token");
-  const [selectedCandidate, setSelectedCandidate] = useState({});
- 
-  const selectionHandler = (candidate) => {
-    setSelectedCandidate(candidate);
-
-    console.log(selectedCandidate);
+  const [wizardStep, setWizardStep] = useState(1);
+  const [newReport, setNewReport] = useState(newReportInitialState);
+  const nextPageHandler = () => {
+    setWizardStep((prevState) => prevState + 1);
   };
-
+  const prevPageHandler = () => {
+    setWizardStep((prevState) => prevState - 1);
+  };
+  const selectCandidateHandler = (candidate) => {
+    setNewReport({
+      ...newReport,
+      candidateId: candidate.id,
+      candidateName: candidate.name,
+    });
+    //console.log(candidate);
+  };
   return (
     <Fragment>
       <Header title={"Reports Administration"} />
       <Container className="mb-5">
         <Row>
           <Col sm={12} md={6} lg={3}>
-            <SelectionSection selectedCandidate={selectedCandidate} />
+            <SelectionSection wizardStep={wizardStep} newReport={newReport} />
           </Col>
           <Col sm={12} md={6} lg={9} className="border-start">
-            <SelectCandidate selectionHandler={selectionHandler} />
+            {wizardStep === 1 && (
+              <SelectCandidate
+                selectCandidateHandler={selectCandidateHandler}
+                newReport={newReport}
+                nextPageHandler={nextPageHandler}
+              />
+            )}
           </Col>
         </Row>
-        <div className="d-flex justify-content-end my-5">
-          <Button disabled>Next</Button>
-        </div>
       </Container>
       <Footer />
     </Fragment>
   );
 };
-
 export default CreateReportPage;
