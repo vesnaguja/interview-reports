2ed33137bd2fea5195408b0b902773d176b43c80
vesnaguja <80161546+vesnaguja@users.noreply.github.com>
2021-09-27 19:12:56 +0200

    App deploy  in GitHub pages

--- src/view/components/Footer/Footer.jsx
+++ src/view/components/Footer/Footer.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 
-
 const Footer = () => {
   return (
-    <Container fluid className= "bg-primary fixed-bottom">
-      <Container className="py-2 text-light">© 2021 Copyright BIT - TEAM "All Pain, No Gain" </Container>
+    <Container fluid className="bg-primary fixed-bottom">
+      <Container className="py-2 text-light">© 2021 Copyright BIT - TEAM | All Pain, No Gain</Container>
     </Container>
   );
 };
